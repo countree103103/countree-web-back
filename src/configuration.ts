71a1683634010ec4720fe03b84dbcd087694c85a
Countree <717662837@qq.com>
2021-12-12 13:33:05 +0800

    引入session

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,22 +1,35 @@
 import { Configuration, App } from '@midwayjs/decorator';
 import { Application } from '@midwayjs/koa';
 import * as bodyParser from 'koa-bodyparser';
+import * as session from 'koa-session';
 import { join } from 'path';
-import * as orm from '@midwayjs/orm'
+import * as orm from '@midwayjs/orm';
 
 @Configuration({
   imports: [
-    orm                             // 加载 orm 组件
-  ],importConfigs: [
-  join(__dirname,'./config/')]})
+    orm, // 加载 orm 组件
+  ],
+  importConfigs: [join(__dirname, './config/')],
+})
 export class ContainerLifeCycle {
   @App()
   app: Application;
 
   async onReady() {
+    this.app.use(
+      session(
+        {
+          key: 'session', // cookie key
+          maxAge: 24 * 3600 * 1000, // 1天
+        },
+        this.app
+      )
+    );
     // bodyparser options see https://github.com/koajs/bodyparser
-    this.app.use(bodyParser({
-      enableTypes: ['json','form','xml','text']
-    }));
+    this.app.use(
+      bodyParser({
+        enableTypes: ['json', 'form', 'xml', 'text'],
+      })
+    );
   }
 }
