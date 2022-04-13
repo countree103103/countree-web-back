b7255649b128cf9e68d927950b0e75f85f1ee05b
Countree <717662837@qq.com>
2022-04-13 10:15:21 +0800

    configuration修改.bootstrap修改.

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,21 +1,16 @@
 import * as defaultConfig from './config/config.default';
 import { Configuration, App } from '@midwayjs/decorator';
-import { Application } from '@midwayjs/koa';
-// import * as bodyParser from 'koa-bodyparser';
+import * as koa from '@midwayjs/koa';
 import koaBody = require('koa-body');
-import * as session from 'koa-session';
 import * as orm from '@midwayjs/orm';
-// import cors = require('koa-cors');
 @Configuration({
-  // imports: [koaFramework, orm],
-  imports: [orm],
+  imports: [koa, orm],
   importConfigs: [{ default: defaultConfig }],
 })
 export class ContainerLifeCycle {
   @App()
-  app: Application;
+  app: koa.Application;
   async onReady(): Promise<void> {
-    this.app.keys = ['52013140'];
     this.app.use(async (ctx, next) => {
       const origin = ctx.header['origin'];
       origin
@@ -26,26 +21,7 @@ export class ContainerLifeCycle {
       ctx.cookies.set('SameSite', 'None');
       await next();
     });
-    // this.app.use(
-    //   cors({
-    //     origin: 'http://localhost:8080',
-    //     credentials: true,
-    //   })
-    // );
     this.app.use(
-      session(
-        {
-          key: 'server_session',
-          maxAge: 24 * 3600 * 1000,
-        },
-        this.app
-      )
-    );
-    // bodyparser options see https://github.com/koajs/bodyparser
-    this.app.use(
-      // bodyParser({
-      //   enableTypes: ['json', 'form', 'xml', 'text'],
-      // })
       koaBody({
         multipart: true,
       })
